cea592e69ce6f84ee3bad09b9ee46f1b5796ca22
Piotr-Presia <148859762+Piotr-Presia@users.noreply.github.com>
2023-11-14 17:11:04 +0000

    q2

--- script/javascript.js
+++ script/javascript.js
@@ -20,3 +20,99 @@ function song() {
 
     document.getElementById("song").innerHTML = song;
 }
+
+function paperRock(id) {
+
+    var winner = "";
+    var userP = "";
+    var compP = "";
+    var compInput;
+    var userInput;
+
+    for (i = 0; i < 3; i++) {
+        input(id);
+    }
+
+    //INPUT
+    function input(id) {
+        userInput = parseInt(prompt("Type 1 for Rock, 2 for Paper and 3 for Scissors"));
+        if (id == "compute") { //CHECK THE BUTTONS ID
+            compInput = (Math.floor(Math.random() * 3) + 1); //PLAY WITH COMPUTER RANDOM CHOICE
+            pvc("PLAYER", "COMPUTER");
+
+        } else {
+            compInput = parseInt(prompt("Type 1 for Rock, 2 for Paper and 3 for Scissors"));
+            pvc("PLAYER 1", "PLAYER 2");
+        }
+    }
+
+    //COMPUTE
+    function pvc(p1, p2) {
+
+        this.p1 = p1;
+        this.p2 = p2;
+
+        //1. Rock 2. Paper 3. Scissors
+
+        if (userInput == 1) {
+            userP = "1 ROCK";
+        } else if (userInput == 2) {
+            userP = "2 PAPER";
+        } else if (userInput == 3) {
+            userP = "3 SCISSORS";
+        }
+
+        if (compInput == 1) {
+            compP = "1 ROCK";
+        } else if (compInput == 2) {
+            compP = "2 PAPER";
+        } else if (compInput == 3) {
+            compP = "3 SCISSORS";
+        }
+
+        if (compInput == userInput) {
+            winner = "DRAW";
+            printC(p1, p2);
+
+        } else if (compInput == 1 && userInput == 2) {
+            winner = p1;  //"PLAYER";
+            printC(p1, p2);
+
+        } else if (compInput == 1 && userInput == 3) {
+            winner = p2;  //"COMPUTER";
+            printC(p1, p2);
+
+        } else if (compInput == 2 && userInput == 1) {
+            winner = p2;  //"COMPUTER";
+            printC(p1, p2);
+
+        } else if (compInput == 2 && userInput == 3) {
+            winner = p1; //"PLAYER";
+            printC(p1, p2);
+
+        } else if (compInput == 3 && userInput == 1) {
+            winner = p1;  //"PLAYER";
+            printC(p1, p2);
+
+        } else if (compInput == 3 && userInput == 2) {
+            winner = p2;  //"COMPUTER";
+            printC(p1, p2);
+
+        } else {
+
+            window.alert("RESULT\n\n" +
+                p1 + " choice: " + userP + "\n" +
+                p2 + " choice: " + compP + "\n\n" +
+                "The winner is: " + winner);
+        }
+    }
+    //PRINT THE RESULT
+    function printC(p1, p2) {
+
+        window.alert("RESULT\n\n" +
+            p1 + " choice: " + userP + "\n" +
+            p2 + " choice: " + compP + "\n\n" +
+            "The winner is: " + winner);
+    }
+
+}
